f6d9671ba3aa3e543f7ce4bf171e846832ad1caf
Syed Sultan <ssa.56.sultan@gmail.com>
2025-07-02 00:36:32 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       vector<int> pair(2,0);
-    for(int i=0; i<nums.size(); i++) {
-        for(int j=i+1; j<nums.size(); j++) {
-            if(nums[i]+nums[j]==target) {
-                pair[0] = i;
-                pair[1] = j;
-                return pair;
+        vector<int> pair(2,0);
+        int start = 0, end = nums.size()-1;
+        while (start < end) {
+            if((nums[start] + nums[end]) < target) {
+                end--;
+            }
+            else if((nums[start] + nums[end]) > target) {
+                start++;
+            }
+            else {
+                pair[0] = nums[start];
+                pair[1] = nums[end];
             }
         }
-    }
-    return pair; 
+        return pair;
     }
 };
