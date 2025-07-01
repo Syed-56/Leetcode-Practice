984ab2caf6afc753f74c0cc6c7815fed28005679
Syed Sultan <ssa.56.sultan@gmail.com>
2025-07-02 02:16:12 +0500

    Time: 0 ms (100%), Space: 28 MB (85.31%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,15 +3,15 @@ public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
         if(n==1)    return nums[0];
-        int freq=0;
+        int freq=1;
         sort(nums.begin(), nums.end());
         
-        for(int i=0; i<n-1; i++) {
+        for(int i=1; i<n; i++) {
             if(nums[i-1] == nums[i])    freq++;
-            else    freq=0;
+            else    freq=1;
 
             if(freq > n/2)  return nums[i];
         }
+        return -1;
     }
-
 };
