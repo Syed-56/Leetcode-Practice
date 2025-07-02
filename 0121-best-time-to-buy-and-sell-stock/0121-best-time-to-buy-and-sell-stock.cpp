7d127dc1df32f9bd44b82b2323cc8085fbab4164
Syed Sultan <ssa.56.sultan@gmail.com>
2025-07-03 02:56:48 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int bestBuy=0, maxProfit=0;
+        int profit;
+        for(int i=1; i<n; i++) {
+            if(prices[i] > bestBuy)  maxProfit = max(profit,maxProfit);
+            bestBuy = min(bestBuy,prices[i]);
+        }
+        return maxProfit;
+    }
+};
