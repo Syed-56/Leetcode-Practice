17abc87849d18f64a5b6ae9de79e08453fdc9257
Syed Sultan <ssa.56.sultan@gmail.com>
2025-07-09 21:08:35 +0500

    Time: 0 ms (100%), Space: 26 MB (91.89%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,6 +5,7 @@ public:
         int start = 0, end = n-1;
         while(start <= end) {
             int mid = start + (end-start)/2;
+            if(mid==0 || mid==n-1)  return nums[mid];
             if(nums[mid+1] != nums[mid] && nums[mid] != nums[mid-1])    return nums[mid];
             if(mid%2 == 0) {  //both sides have even elements
                 if(nums[mid] == nums[mid+1]) {      //single element exist in right half
