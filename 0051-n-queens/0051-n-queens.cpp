9353efa8920da711d0254a8776513a04f06961db
Syed Sultan <ssa.56.sultan@gmail.com>
2025-07-28 17:30:22 +0500

    Time: 3 ms (65.27%), Space: 10.3 MB (55.06%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isSafe(vector<string>& board, int row, int col, int n) {
+    //horizontal check
+    for(int i=0; i<row; i++) {
+        if(board[i][col]=='Q')  return false;
+    }
+    //vertical check
+    for(int j=0; j<col; j++) {
+        if(board[row][j]=='Q')  return false;
+    }
+
+    //left diagonal
+    for(int i=row,j=col; i>=0 && j>=0; i--,j--) {
+        if(board[i][j]=='Q')    return false;
+    }
+    //right diagonal
+    for(int i=row,j=col; i>=0 && j<n; i--,j++) {
+        if(board[i][j]=='Q')    return false;
+    }
+    return true;
+}
+
+void backTrack(vector<vector<string>>& ans, vector<string>& board, int row, int n) {
+    if(row==n) {
+        ans.push_back(board);
+        return;
+    }
+
+    for(int col=0; col<n; col++) {
+        if(isSafe(board,row,col,n)) {
+            board[row][col] = 'Q';
+            backTrack(ans,board,row+1,n);
+            board[row][col] = '.';
+        }
+    }
+}
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> allPairs;
+        vector<string> board(n,string(n,'.'));  //initialize whole board with ....
+        backTrack(allPairs,board,0,n);
+        return allPairs;
+    }
+};
